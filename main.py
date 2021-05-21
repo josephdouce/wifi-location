72a07539032a74aee319d57f054633b1b7ebbc89
joseph douce <josephdouce@gmail.com>
2021-05-21 09:55:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -191,7 +191,6 @@ def webhook():
     if request.method == 'POST':
         repo = git.Repo('./')
         origin = repo.remotes.origin
-        print(origin)
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
     else:
